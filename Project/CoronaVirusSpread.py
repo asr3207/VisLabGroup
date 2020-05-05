fb29ab3073c54f3f014fa1443c0fa5b1cbb5b6d2
Noah Gillespie <59939752+Suwanee1@users.noreply.github.com>
2020-05-05 16:21:02 -0400

    Final demo first commit

--- Project/CoronaVirusSpread.py
+++ Project/CoronaVirusSpread.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 df = pd.read_csv('CoronaVirusCasesUpdated.csv')
 df2 = pd.read_csv('NC-Coronavirus - Sheet1.csv')
+df3 = pd.read_csv('CoronaVirusCasesWeekly.csv')
 df2['Date'] = pd.to_datetime(df2['Date'])
 
 app = dash.Dash()
@@ -53,6 +54,39 @@ fig3.update_layout(
 LineChartData = [go.Scatter(x=df2['Date'], y=df2['Cumulative'], mode='lines',
                             name='Cumulative number of cases per day in North Carolina')]
 
+# animated map of number of cases
+figA = go.Figure(data=go.Choropleth(
+    locations=df3['Code'],
+    z=df3['March1'].astype(float),
+    locationmode='USA-states',
+    colorscale='bupu',
+    colorbar_title="Confirmed Cases of Covid-19",
+    zauto=False,
+    zmin=0,
+    zmax=300000
+),
+    frames=[go.Frame(data=go.Choropleth(z=df3['March1'].astype(float), text="Week 1 of March")),
+            go.Frame(data=go.Choropleth(z=df3['March2'].astype(float), text="Week 2 of March")),
+            go.Frame(data=go.Choropleth(z=df3['March3'].astype(float), text="Week 3 of March")),
+            go.Frame(data=go.Choropleth(z=df3['March4'].astype(float), text="Week 4 of March")),
+            go.Frame(data=go.Choropleth(z=df3['April1'].astype(float), text="Week 1 of April")),
+            go.Frame(data=go.Choropleth(z=df3['April2'].astype(float), text="Week 2 of April")),
+            go.Frame(data=go.Choropleth(z=df3['April3'].astype(float), text="Week 3 of April")),
+            go.Frame(data=go.Choropleth(z=df3['April4'].astype(float), text="Week 4 of April")),
+            go.Frame(data=go.Choropleth(z=df3['May1'].astype(float), text="Week 1 of May")),
+            ]
+
+)
+figA.update_layout(
+    title_text='Confirmed Coronavirus Cases',
+    geo_scope='usa',
+    updatemenus=[dict(
+        type="buttons",
+        buttons=[dict(label="Play",
+                      method="animate",
+                      args=[None])])],
+)
+
 app.layout = html.Div(children=[
     html.H1(children='Visualization of the Corona virus',
             style={
@@ -97,6 +131,12 @@ app.layout = html.Div(children=[
                       title='Cumulative number of cases in North Carolina per day 03/01/2020 - 04/18/2020',
                       xaxis_title="Date", yaxis_title="Number of Cases")
               }
+              ),
+    html.Hr(style={'color': '#7FDBFF'}),
+    html.H3('Animated Chart', style={'color': '#df1e56'}),
+    html.Div('This animated chart shows the number of cases updated once a week from March 1st to May 1st'),
+    dcc.Graph(id='graph6',
+              figure=figA
               ),
 ])
 
